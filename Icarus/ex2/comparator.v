b38f5492bd257823ee305519b6d5096d00aa9428
YixingShen <kkman3651878@hotmail.com>
2020-08-31 23:24:09 +0800

    update

---
+++ Icarus/ex2/comparator.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 100ps
+
+module comparator(
+	input x,
+	input y,
+	output z
+);
+udp_syntax udp (z, x, y);
+endmodule
+
+// User Defined Primitive
+primitive udp_syntax (out, in1, in2);
+	output out;
+	input in1,in2;
+ 
+table
+// in1  in2  : out
+    0    0   : 1; 
+    0    1   : 0; 
+    1    0   : 0; 
+    1    1   : 1; 
+endtable
+endprimitive
